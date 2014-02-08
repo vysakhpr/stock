cc35954cc838622fe49016ab625072f867e4eabb
vysakhpr <my_mail_is_buzy@yahoo.com.au>
2014-02-08 13:11:27 +0530

    Added calulations on quantity remaining and assigned

--- app/controllers/lab_controller.rb
+++ app/controllers/lab_controller.rb
@@ -7,11 +7,13 @@ class LabController < ApplicationController
   def create
   	@lab=current_user.labs.build(params[:lab])
   	unless @lab.quantity.nil?
-  		if @lab.office.quantity < @lab.quantity
+  		if (@lab.office.quantity-@lab.office.quantity_assigned) < @lab.quantity
   			flash[:error]="Quantity over full size"
   			redirect_to :back
   		elsif @lab.save
   			flash[:info]="Successively Assigned"
+        diff=@lab.office.quantity_assigned+@lab.quantity
+        @lab.office.update_attribute(:quantity_assigned,diff)
   			redirect_to root_url
   		end	
   	else
--- app/models/office.rb
+++ app/models/office.rb
@@ -5,7 +5,7 @@ class Office < ActiveRecord::Base
   validates_presence_of :date,:department,:description,:name,:price_unit,:quantity,:total_price,:voucher_no
   validates_uniqueness_of :voucher_no
   validates_numericality_of :quantity, :integer_only => true, :greater_than=> 0
-  validates_numericality_of :quantity_assigned,:integer_only=> true, :greater_than_or_equal_to =>0
+  validates_numericality_of :not_assigned,:integer_only=> true, :greater_than_or_equal_to =>0
   validates :price_unit, numericality: {:greater_than_or_equal_to=>0}
   validates :total_price, numericality: {:greater_than_or_equal_to=>0}
 end
